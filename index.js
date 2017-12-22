4fea9b1f6eba6bdc8bd842b16ce68e588de6f072
devsadventure <margaret.the.adventurer@gmail.com>
2017-12-22 17:18:00 +0900

    click event 완성

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ const listEl = document.querySelector('#todo-list');
 addButtonEl.addEventListener('click', e => {
   const itemEl = document.createElement('div');
 
+  // 삭제 버튼 만들기
+  const x = document.createElement('div');
+
   itemEl.textContent = inputEl.value;
+  itemEl.appendChild(x);
   listEl.appendChild(itemEl);
+
   inputEl.value = '';
+
+  itemEl.addEventListener('click', e => {
+    if (itemEl.classList.contains('complete')) {
+      itemEl.classList.remove('complete');
+    } else {
+      itemEl.classList.add('complete');
+    }
+  })
+
+  const removeButtonEl = document.createElement('div');
+  removeButtonEl.textContent = 'x';
 });
